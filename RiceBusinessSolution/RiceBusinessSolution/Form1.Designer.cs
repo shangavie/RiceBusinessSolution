819c412c58a4793bf36790ef3ad50223d9efe948
it16167124@my.sliit.lk <it16167124@my.sliit.lk>
2020-11-18 19:02:24 +0530

    rice business solution project

---
+++ RiceBusinessSolution/RiceBusinessSolution/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RiceBusinessSolution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txt4kg = new System.Windows.Forms.TextBox();
+            this.txt2Kg = new System.Windows.Forms.TextBox();
+            this.txt1Kg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(100, 30);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 20);
+            this.txtWeight.TabIndex = 0;
+            this.txtWeight.TextChanged += new System.EventHandler(this.txtWeight_TextChanged);
+            this.txtWeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtWeight_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Weight of Order";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kg";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(209, 81);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(94, 32);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txt4kg
+            // 
+            this.txt4kg.Location = new System.Drawing.Point(100, 142);
+            this.txt4kg.Name = "txt4kg";
+            this.txt4kg.Size = new System.Drawing.Size(100, 20);
+            this.txt4kg.TabIndex = 4;
+            // 
+            // txt2Kg
+            // 
+            this.txt2Kg.Location = new System.Drawing.Point(100, 178);
+            this.txt2Kg.Name = "txt2Kg";
+            this.txt2Kg.Size = new System.Drawing.Size(100, 20);
+            this.txt2Kg.TabIndex = 5;
+            // 
+            // txt1Kg
+            // 
+            this.txt1Kg.Location = new System.Drawing.Point(100, 217);
+            this.txt1Kg.Name = "txt1Kg";
+            this.txt1Kg.Size = new System.Drawing.Size(100, 20);
+            this.txt1Kg.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "No of 4Kg Bags";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "No of 2Kg Bags";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "No of 1Kg Bags";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 289);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt1Kg);
+            this.Controls.Add(this.txt2Kg);
+            this.Controls.Add(this.txt4kg);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWeight);
+            this.Name = "Form1";
+            this.Text = "Calculate Rice Bags";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txt4kg;
+        private System.Windows.Forms.TextBox txt2Kg;
+        private System.Windows.Forms.TextBox txt1Kg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RiceBusinessSolution/RiceBusinessSolution/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RiceBusinessSolution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txt4kg.Enabled = true;
+            txt2Kg.Enabled = true;
+            txt1Kg.Enabled = true;
+
+            txt4kg.Text = "";
+            txt2Kg.Text = "";
+            txt1Kg.Text = "";
+
+            double weight = Convert.ToDouble(txtWeight.Text);
+
+            if(weight >= 0.5)
+            {
+                if(weight-Math.Truncate(weight) == 0.5 || weight - Math.Truncate(weight) == 0)
+                {
+                    int weight4bags = (int)(weight / 4);
+
+                    if (weight4bags >= 1)
+                    {
+                        txt4kg.Text = weight4bags.ToString();
+                    }
+
+                    int weight2bags = (int)(weight % 4.0) / 2;
+                    if (weight2bags >= 1)
+                    {
+                        txt2Kg.Text = weight2bags.ToString();
+                    }
+
+                    double weight1bags = (weight - weight4bags * 4) - weight2bags * 2;
+
+                    if (weight1bags == 0.5 || weight1bags == 1)
+                    {
+                        txt1Kg.Text = "1";
+                    }
+                    else if (weight1bags == 1.5)
+                    {
+                        txt1Kg.Text = "2";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("The order is rejected");
+                }
+                txt4kg.Enabled = false;
+                txt2Kg.Enabled = false;
+                txt1Kg.Enabled = false;                
+            }
+
+        }
+
+        private void txtWeight_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8 && e.KeyChar != 46))
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if(e.KeyChar == 46)
+            {
+                if((sender as TextBox).Text.IndexOf(e.KeyChar) != -1)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void txtWeight_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
